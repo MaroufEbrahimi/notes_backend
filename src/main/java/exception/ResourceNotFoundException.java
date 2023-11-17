45c5fc28bcb1656238f8b150a2099df52d98159e
Marouf <maroufibrahimi7264@gmail.com>
2023-11-17 15:38:14 +0430

    initial commit

--- src/main/java/exception/ResourceNotFoundException.java
+++ src/main/java/exception/ResourceNotFoundException.java
@@ -3,8 +3,6 @@ package exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serial;
-
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
