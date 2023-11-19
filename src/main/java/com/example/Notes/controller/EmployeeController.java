ebd423007ceaee7cb47f1f3bf1d65ff4762edde5
Marouf <maroufibrahimi7264@gmail.com>
2023-11-19 14:15:58 +0430

    initial commit

--- src/main/java/com/example/Notes/controller/EmployeeController.java
+++ src/main/java/com/example/Notes/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.Notes.controller;
 
 import com.example.Notes.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.example.Notes.repository.EmployeeRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
