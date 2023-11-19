9518cdaa8dc5fad92f4d4b81f7a1f5d3718b55d3
Marouf <maroufibrahimi7264@gmail.com>
2023-11-19 14:16:19 +0430

    initial commit

--- src/main/java/com/example/Notes/repository/EmployeeRepository.java
+++ src/main/java/com/example/Notes/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.example.Notes.repository;
 
 import com.example.Notes.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
